1e2e3b18a573c132715706c3d7b22ba5d282218b
Vitaly Tarasenko <vitaliyt@interlink-ua.com>
2014-12-28 14:34:53 +0200

    add setup interface

--- lib/swat_tw.rb
+++ lib/swat_tw.rb
@@ -3,6 +3,11 @@ module Swat
     require 'swat/test_world/base'
     require 'swat/test_world/config'
 
+    mattr_reader :config
+
+    def setup(opts)
+      @config = Config.new(opts)
+    end
 
   end
 end
