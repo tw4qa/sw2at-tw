d93eaf7bd0a6d27559fcf3d326661ac5c7094ec6
Vitaly Tarasenko <vitaliyt@interlink-ua.com>
2014-12-29 10:27:51 +0200

    correct path

--- lib/swat/test_world/generators/new_generator.rb
+++ lib/swat/test_world/generators/new_generator.rb
@@ -4,7 +4,7 @@ module Swat
   module TestWorld
     module Generators
       class NewGenerator < ::Rails::Generators::NamedBase
-        source_root File.expand_path('../templates', __FILE__)
+        source_root File.expand_path('../../templates', __FILE__)
 
         desc 'Generates Test World files'
         def create_tw
